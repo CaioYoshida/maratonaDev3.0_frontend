c3565612748355193f6db7e32354a3d91c611982
Caio Yoshida <caio.yoshida@yahoo.com.br>
2020-02-18 10:58:15 -0300

    chore: updating server.js / style.css / index.html

--- server.js
+++ server.js
@@ -5,15 +5,53 @@ const server = express();
 // configurar o servidor para apresentar arquivos estáticos
 server.use(express.static('public'));
 
+//Habilitar body do formulário
+server.use(express.urlencoded({ extended: true }));
+
 // configurando a template engine
 const nunjucks = require('nunjucks');
 nunjucks.configure('./', {
-  express: server
-})
+  express: server,
+  noCache: true,
+});
+
+const donors = [
+  {
+    name: "Diego Fernandes",
+    blood: "AB+"
+  },
+  {
+    name: "Cleiton Souza",
+    blood: "B+"
+  },
+  {
+    name: "Robson Marques",
+    blood: "A+"
+  },
+  {
+    name: "Mayk Brito",
+    blood: "O+"
+  },
+]
 
 // configurar a apresentação da pagina
 server.get('/', function(req, res) {
-  return res.render('index.html');
+  return res.render('index.html', { donors });
+})
+
+server.post('/', function(req, res) {
+  // pegar dados no formulário
+  const name = req.body.name;
+  const email = req.body.email;
+  const blood = req.body.blood;
+
+  // coloco valores dentro do array
+  donors.push({
+    name,
+    blood,
+  });
+
+  return res.redirect('/');
 })
 
 // ligar o servidor e permitir o acesso na porta 3000
